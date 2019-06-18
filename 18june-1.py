4a28a256e0f3b17c62f5db6239554f2c128a7f2e
sk4201 <16tec2cs024@vgu.ac.in>
2019-06-18 20:56:52 +0530

    numpy library

---
+++ 18june-1.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python3
+
+
+import   numpy    as np
+
+# creating  1D  array
+
+a=np.array([2,3,5,79])
+b=np.array([12,13,15,179])
+
+#  adding  same  D  array
+c=a+b
+print(c)
+#   multi   
+m=a*b
+print(m)
+# add  particular  value in each  element of  array
+a5=a+5
+print(a5)
